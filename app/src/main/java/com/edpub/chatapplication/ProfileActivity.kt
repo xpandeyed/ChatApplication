965e4817bf3609942b59fba687d841295dd08e48
xpandeyed <lalbiharipandeyg@gmail.com>
2022-01-14 13:58:29 +0530

    Layouts added/changed.

--- app/src/main/java/com/edpub/chatapplication/ProfileActivity.kt
+++ app/src/main/java/com/edpub/chatapplication/ProfileActivity.kt
@@ -2,6 +2,9 @@ package com.edpub.chatapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +13,9 @@ class ProfileActivity : AppCompatActivity() {
 
         setSupportActionBar(findViewById(R.id.tbProfile))
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        findViewById<TextView>(R.id.tvUserName).text = FirebaseAuth.getInstance().currentUser?.displayName
+        findViewById<TextView>(R.id.tvEmail).text = FirebaseAuth.getInstance().currentUser?.email
+        Glide.with(this).load(FirebaseAuth.getInstance().currentUser?.photoUrl).into(findViewById(R.id.ivProfilePicture))
     }
 }
